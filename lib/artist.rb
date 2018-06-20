0dc4f7ec104ca8055a33e25a7a0da17bc00a12fb
Trevor Russell <truss.42@gmail.com>
2018-06-20 00:54:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,10 +7,15 @@ class Artist
   end
   
   def add_song(song)
+    @song << song
     song.artist = self
   end
   
   def add_song_by_name(song)
     song = Song.new(song)
     self.add_song(song)
+  end
+  def self.song_count
+    @songs.count 
+  end
 end
